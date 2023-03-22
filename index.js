1747e474ed8ef9e6066735d1bd120706235aeef1
Julian Santana <jkamiphi@gmail.com>
2023-03-22 11:46:59 -0400

    feat: :sparkles: VAST: Add other creative

--- index.js
+++ index.js
@@ -46,32 +46,10 @@ app.get('/vast/:contentId', (req, res) => {
   const linear = creative.ele('Linear');
   linear.ele('Duration', {}).dat('00:00:16');
 
-  const trackingEvents = linear.ele('TrackingEvents');
-
-  trackingEvents.ele('Tracking', {
-    event: 'start',
-    offset: '00:00:00'
-  }).dat('http://example.com/start');
-  trackingEvents.ele('Tracking', {
-    event: 'firstQuartile',
-    offset: '00:00:08'
-  }).dat('http://example.com/firstQuartile');
-  trackingEvents.ele('Tracking', {
-    event: 'midpoint',
-    offset: '00:00:15'
-  }).dat('http://example.com/midpoint');
-  trackingEvents.ele('Tracking', {
-    event: 'thirdQuartile',
-    offset: '00:00:22'
-  }).dat('http://example.com/thirdQuartile');
-  trackingEvents.ele('Tracking', {
-    event: 'complete',
-    offset: '00:00:30'
-  }).dat('http://example.com/complete');
-
-  const iconClicks = linear.ele('IconClicks');
-
-  iconClicks.ele('IconClickTracking', {}).dat('https://development.palenquetv.com/images/icons/icon-128x128.png')
+  const icons = linear.ele('Icons');
+  const icon = icons.ele('Icon', {'height': 'auto', 'xPosition': 'right', 'yPosition': 'top'});
+  icon.ele('StaticResource', {'creativeType': 'image/png'})
+    .dat('https://development.palenquetv.com/images/icons/icon-128x128.png')
 
   const videoClicks = linear.ele('VideoClicks');
 
@@ -92,6 +70,40 @@ app.get('/vast/:contentId', (req, res) => {
       'minBitrate': '360',
     })
     .dat('https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro.mp4');
+
+  // other creative
+  const creative2 = creatives.ele('Creative', {
+    id: req.params.contentId + '2',
+    sequence: '2'
+  });
+
+  const linear2 = creative2.ele('Linear', {'skipoffset': '00:00:15'});
+  linear2.ele('Duration', {}).dat('00:00:56');
+
+  const icons2 = linear2.ele('Icons');
+  const icon2 = icons2.ele('Icon', {'height': 'auto', 'xPosition': 'right', 'yPosition': 'top'});
+  icon2.ele('StaticResource', {'creativeType': 'image/png'})
+    .dat('https://development.palenquetv.com/images/icons/icon-128x128.png')
+
+  const videoClicks2 = linear2.ele('VideoClicks');
+
+  videoClicks2.ele('ClickTracking', {}).dat('https://iabtechlab.com');
+
+  const mediaFiles2 = linear2.ele('MediaFiles');
+  
+  mediaFiles2
+    .ele('MediaFile', {
+      'id': '5241', 
+      'type': 'video/mp4', 
+      'bitrate': '500', 
+      'delivery': 'streaming', 
+      'maintainAspectRatio': '1',
+      'codec': '0',
+      'scalable': '1',
+      'maxBitrate': '1000',
+      'minBitrate': '360',
+    })
+    .dat('https://stream.mux.com/oyqTzCWKNrMDHpMmCjUJRppmZbybuSPhNgchvCoL3Qk.m3u8');
 
   res.set('Content-Type', 'text/xml');
   res.send(vast.end({pretty: true}));
