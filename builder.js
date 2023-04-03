4ae7b529dd21c814c8d3c936a64e1e93c4033f1a
Julian Santana <jkamiphi@gmail.com>
2023-04-03 15:05:13 -0400

    feat: :art: VAST: Add builder

---
+++ builder.js
@@ -0,0 +1,83 @@
+const { create } = require('xmlbuilder2');
+
+function formatTime(n) {
+  const seconds = n % 60;
+  const minutes = Math.floor(n / 60) % 60;
+  const hours = Math.floor(n / 3600);
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+class AdsBuilder {
+  constructor(title) {
+    this.root = create({
+      version: '1.0',
+      encoding: 'UTF-8'
+    }).ele("vmap:VMAP", {
+      "xmlns:vmap": "http://www.iab.net/videosuite/vmap",
+      "version": "1.0"
+    });
+
+    this.adSystemTitle = title;
+  }
+
+  addAdToVAST(ad) {
+    const {
+      duration,
+      mediaFileUri,
+      offset,
+      title,
+      description = "",
+      type = "video/mp4",
+      breakId = "break-1",
+      sourceId = "source-1"
+    } = ad;
+
+    const adBreakElement = this.root.ele("vmap:AdBreak", {
+      breakType: "linear",
+      timeOffset: offset === 0 ? "start" : offset === duration ? "end" : formatTime(offset),
+      breakId: breakId,
+    });
+
+    const adSourceElement = adBreakElement.ele("vmap:AdSource", {
+      allowMultipleAds: true,
+      followRedirects: true,
+      id: sourceId,
+    });
+
+    const vastAdData = adSourceElement.ele("vmap:VASTAdData");
+
+    const vastAdDoc = vastAdData.ele("VAST", {
+      'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+      'xsi:noNamespaceSchemaLocation': 'vast.xsd',
+      version: '3.0'
+    });
+
+    const adElement = vastAdDoc.ele("Ad");
+    const inline = adElement.ele("InLine");
+
+    inline.ele("AdSystem").dat(this.adSystemTitle);
+    inline.ele("AdTitle").dat(title);
+    inline.ele("Description").dat(description);
+    inline.ele("Impression").dat("http://example.com/impression");
+
+    const creatives = inline.ele("Creatives");
+    const creative = creatives.ele("Creative");
+
+    const linear = creative.ele("Linear");
+    linear.ele("Duration").dat(formatTime(duration));
+
+    const mediaFiles = linear.ele("MediaFiles");
+    mediaFiles.ele("MediaFile", {
+      id: "1",
+      delivery: "progressive",
+      type: type,
+      bitrate: "1000"
+    }).dat(mediaFileUri);
+  }
+
+  get() {
+    return this.root.end({ prettyPrint: true });
+  }
+}
+
+module.exports = AdsBuilder;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const xmlbuilder = require('xmlbuilder2');
+const AdsBuilder = require('./builder');
 const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -13,66 +13,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/vast/:contentId', (req, res) => {
-  const vast = xmlbuilder.create({
-    version: '1.0',
-    encoding: 'UTF-8'
+  const builder = new AdsBuilder("simple-ads-server")
+
+  builder.addAdToVAST({
+    duration: 30,
+    mediaFileUri: "https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro.mp4",
+    offset: 12,
+    title: "Simple Ad",
+    description: "Description",
+    type: "video/mp4",
+    breakId: "break-1",
+    sourceId: "source-1"
   })
-    .ele('VAST', {
-      'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-      'xsi:noNamespaceSchemaLocation': 'vast.xsd',
-      version: '3.0'
-    });
-
-  const ad = vast.ele('Ad', {
-    id: req.params.contentId,
-    sequence: '1'
-  });
-
-  const inline = ad.ele('InLine');
-
-  inline.ele('AdSystem', {version: '4.0'}).txt('simplevastserver-jkamiphi');
-  inline.ele('AdTitle', {}).dat(`Ad for content ${req.params.contentId}`);
-  inline.ele('Description').dat('This is sample companion ad tag with Linear ad tag.')
-  inline.ele('Impression', { id: 'impression-1' }).dat('http://example.com/impression');
-  inline.ele('Error').dat('http://example.com/error');
-
-  const creatives = inline.ele('Creatives');
-
-  const creative = creatives.ele('Creative', {
-    id: req.params.contentId,
-    sequence: '1'
-  });
-
-  const linear = creative.ele('Linear');
-  linear.ele('Duration', {}).dat('00:00:16');
-
-  const icons = linear.ele('Icons');
-  const icon = icons.ele('Icon', {'height': 'auto', 'xPosition': 'right', 'yPosition': 'top'});
-  icon.ele('StaticResource', {'creativeType': 'image/png'})
-    .dat('https://development.palenquetv.com/images/icons/icon-128x128.png')
-
-  const videoClicks = linear.ele('VideoClicks');
-
-  videoClicks.ele('ClickTracking', {}).dat('https://iabtechlab.com');
-
-  const mediaFiles = linear.ele('MediaFiles');
-  
-  mediaFiles
-    .ele('MediaFile', {
-      'id': '5241', 
-      'type': 'video/mp4', 
-      'bitrate': '500', 
-      'delivery': 'progressive', 
-      'maintainAspectRatio': '1',
-      'codec': '0',
-      'scalable': '1',
-      'maxBitrate': '1000',
-      'minBitrate': '360',
-    })
-    .dat('https://iab-publicfiles.s3.amazonaws.com/vast/VAST-4.0-Short-Intro.mp4');
 
   res.set('Content-Type', 'text/xml');
-  res.send(vast.end({pretty: true}));
+  res.send(builder.get());
 });
 
 app.all("*", (req, res) => {
